4eec03d57895e4a2a0c9ab1029caa8314825e15c
Ojas Bendale <ojasbendale18200@gmail.com>
2023-09-07 11:24:17 +0530

    updating add task ui

--- backend/app.js
+++ backend/app.js
@@ -31,6 +31,10 @@ app.use("/api/auth", authRoutes);
 app.use("/api/tasks", taskRoutes);
 app.use("/api/profile", profileRoutes);
 
+app.get("/", (req, res) => {
+  res.send("hello");
+});
+
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.resolve(__dirname, "../frontend/build")));
   app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "../frontend/build/index.html")));
--- frontend/src/pages/Task.jsx
+++ frontend/src/pages/Task.jsx
@@ -17,19 +17,24 @@ const Task = () => {
   const mode = taskId === undefined ? "add" : "update";
   const [task, setTask] = useState(null);
   const [formData, setFormData] = useState({
-    description: ""
+    description: "",
+    assignedTo: "",
+    priority: "",
+    deadline: "",
   });
   const [formErrors, setFormErrors] = useState({});
-
 
   useEffect(() => {
     document.title = mode === "add" ? "Add task" : "Update Task";
   }, [mode]);
 
-
   useEffect(() => {
     if (mode === "update") {
-      const config = { url: `/tasks/${taskId}`, method: "get", headers: { Authorization: authState.token } };
+      const config = {
+        url: `/tasks/${taskId}`,
+        method: "get",
+        headers: { Authorization: authState.token },
+      };
       fetchData(config, { showSuccessToast: false }).then((data) => {
         setTask(data.task);
         setFormData({ description: data.task.description });
@@ -37,77 +42,149 @@ const Task = () => {
     }
   }, [mode, authState, taskId, fetchData]);
 
-
-
-  const handleChange = e => {
+  const handleChange = (e) => {
     setFormData({
-      ...formData, [e.target.name]: e.target.value
+      ...formData,
+      [e.target.name]: e.target.value,
     });
-  }
+  };
 
-  const handleReset = e => {
+  const handleReset = (e) => {
     e.preventDefault();
     setFormData({
-      description: task.description
+      description: task.description,
     });
-  }
+  };
 
-  const handleSubmit = e => {
+  const handleSubmit = (e) => {
     e.preventDefault();
     const errors = validateManyFields("task", formData);
     setFormErrors({});
 
     if (errors.length > 0) {
-      setFormErrors(errors.reduce((total, ob) => ({ ...total, [ob.field]: ob.err }), {}));
+      setFormErrors(
+        errors.reduce((total, ob) => ({ ...total, [ob.field]: ob.err }), {})
+      );
       return;
     }
 
     if (mode === "add") {
-      const config = { url: "/tasks", method: "post", data: formData, headers: { Authorization: authState.token } };
+      const config = {
+        url: "/tasks",
+        method: "post",
+        data: formData,
+        headers: { Authorization: authState.token },
+      };
+      fetchData(config).then(() => {
+        navigate("/");
+      });
+    } else {
+      const config = {
+        url: `/tasks/${taskId}`,
+        method: "put",
+        data: formData,
+        headers: { Authorization: authState.token },
+      };
       fetchData(config).then(() => {
         navigate("/");
       });
     }
-    else {
-      const config = { url: `/tasks/${taskId}`, method: "put", data: formData, headers: { Authorization: authState.token } };
-      fetchData(config).then(() => {
-        navigate("/");
-      });
-    }
-  }
-
+  };
 
   const fieldError = (field) => (
-    <p className={`mt-1 text-pink-600 text-sm ${formErrors[field] ? "block" : "hidden"}`}>
-      <i className='mr-2 fa-solid fa-circle-exclamation'></i>
+    <p
+      className={`mt-1 text-pink-600 text-sm ${
+        formErrors[field] ? "block" : "hidden"
+      }`}
+    >
+      <i className="mr-2 fa-solid fa-circle-exclamation"></i>
       {formErrors[field]}
     </p>
-  )
+  );
 
   return (
     <>
       <MainLayout>
-        <form className='m-auto my-16 max-w-[1000px] bg-white p-8 border-2 shadow-md rounded-md'>
+        <form className="m-auto my-16 max-w-[1000px] bg-white p-8 border-2 shadow-md rounded-md">
           {loading ? (
             <Loader />
           ) : (
             <>
-              <h2 className='text-center mb-4'>{mode === "add" ? "Add New Task" : "Edit Task"}</h2>
+              <h2 className="text-center mb-4">
+                {mode === "add" ? "Add New Task" : "Edit Task"}
+              </h2>
               <div className="mb-4">
                 <label htmlFor="description">Description</label>
-                <Textarea type="description" name="description" id="description" value={formData.description} placeholder="Write here.." onChange={handleChange} />
+                <Textarea
+                  type="description"
+                  name="description"
+                  id="description"
+                  value={formData.description}
+                  placeholder="Write here.."
+                  onChange={handleChange}
+                />
+                <div className="flex flex-col gap-2 mt-4">
+                  <label htmlFor="Assignedto">AssignedTo</label>
+                  <select
+                    className="py-3 outline-2 border-2"
+                    onChange={handleChange}
+                    name="assignedTo"
+                  >
+                    <option value="saurabh">Saurabh</option>
+                    <option value="aseem">Aseem</option>
+                    <option value="namrata">namrata</option>
+                  </select>
+                </div>
+                <div className="flex flex-col gap-2 mt-4">
+                  <label htmlFor="Assignedto">Priority</label>
+                  <select
+                    className="py-3 outline-2 border-2"
+                    onChange={handleChange}
+                    name="priority"
+                  >
+                    <option value="low">Low</option>
+                    <option value="medium">Medium</option>
+                    <option value="high">High</option>
+                  </select>
+                </div>
+                <div className="flex flex-col gap-2 mt-4">
+                  <label htmlFor="Assignedto">Deadline</label>
+                  <input
+                    type="date"
+                    onChange={handleChange}
+                    name="deadline"
+                    className="py-3 outline-2 border-2 "
+                  />
+                </div>
                 {fieldError("description")}
               </div>
 
-              <button className='bg-primary text-white px-4 py-2 font-medium hover:bg-primary-dark' onClick={handleSubmit}>{mode === "add" ? "Add task" : "Update Task"}</button>
-              <button className='ml-4 bg-red-500 text-white px-4 py-2 font-medium' onClick={() => navigate("/")}>Cancel</button>
-              {mode === "update" && <button className='ml-4 bg-blue-500 text-white px-4 py-2 font-medium hover:bg-blue-600' onClick={handleReset}>Reset</button>}
+              <button
+                className="bg-primary text-white px-4 py-2 font-medium hover:bg-primary-dark"
+                onClick={handleSubmit}
+              >
+                {mode === "add" ? "Add task" : "Update Task"}
+              </button>
+              <button
+                className="ml-4 bg-red-500 text-white px-4 py-2 font-medium"
+                onClick={() => navigate("/")}
+              >
+                Cancel
+              </button>
+              {mode === "update" && (
+                <button
+                  className="ml-4 bg-blue-500 text-white px-4 py-2 font-medium hover:bg-blue-600"
+                  onClick={handleReset}
+                >
+                  Reset
+                </button>
+              )}
             </>
           )}
         </form>
       </MainLayout>
     </>
-  )
+  );
 }
 
 export default Task
