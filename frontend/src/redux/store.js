ea35c143bb130ca4a043647b982e03587916f078
Ojas Bendale <ojasbendale18200@gmail.com>
2023-09-14 16:07:32 +0530

    fixed redux

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,14 +1,8 @@
 import { applyMiddleware, createStore, compose } from "redux";
 import thunk from "redux-thunk";
-import { combineReducers } from "redux";
 import rootReducer from "./reducers";
-import authReducer from "./authReducer";
 
 const middleware = [thunk];
-
-const rootReducer = combineReducers({
-  authReducer,
-});
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
